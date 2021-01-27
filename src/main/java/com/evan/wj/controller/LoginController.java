07b9d17e29171d16679b1df4f47878aa24129840
韩启龙 <hanqilong@bailitop.com>
2021-01-27 19:38:06 +0800

    Initial commit

---
+++ src/main/java/com/evan/wj/controller/LoginController.java
@@ -0,0 +1,39 @@
+package com.evan.wj.controller;
+
+import com.evan.wj.pojo.User;
+import com.evan.wj.result.Result;
+import com.evan.wj.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.Objects;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    UserService userService;
+
+    @CrossOrigin
+    @PostMapping(value = "api/login")
+    @ResponseBody
+    public Result login(@RequestBody User requestUser) {
+        // 对 html 标签进行转义，防止 XSS 攻击
+        String username = requestUser.getUsername();
+        username = HtmlUtils.htmlEscape(username);
+
+        User user = userService.get(username, requestUser.getPassword());
+        if (null == user) {
+            String message = "账号密码错误";
+            System.out.println("test");
+            return new Result(400);
+        } else {
+            return new Result(200);
+        }
+    }
+}
---
+++ src/main/java/com/evan/wj/service/UserService.java
@@ -0,0 +1,30 @@
+package com.evan.wj.service;
+
+import com.evan.wj.dao.UserDao;
+import com.evan.wj.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public boolean isExist(String username) {
+        User user = getByName(username);
+        return null!=user;
+    }
+
+    public User getByName(String username) {
+        return userDao.findByUsername(username);
+    }
+
+    public User get(String username, String password){
+        return userDao.getByUsernameAndPassword(username, password);
+    }
+
+    public void add(User user) {
+        userDao.save(user);
+    }
+}
